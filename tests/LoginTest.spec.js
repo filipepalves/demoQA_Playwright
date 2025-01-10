b8a63332f9d6b9318cfb9a3205975636df9aab5b
filipepalves <filipepretoalves@gmail.com>
2025-01-10 15:16:07 +0000

    starting project

--- tests/LoginTest.spec.js
+++ tests/LoginTest.spec.js
@@ -1,7 +1,8 @@
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
 test('Login test', async ({page}) =>
 {
 await page.goto('https://demoqa.com/login');
 await expect(page).toHaveTitle('DEMOQA');
+
 })
